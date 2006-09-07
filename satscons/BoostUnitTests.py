99de21b3b8c3776223f7afc3289648cb00c24e72
sbund <sbund@wiback.org>
2006-09-07 08:31:11 +0000

    Fixed BoostUnitTest target

--- satscons/BoostUnitTests.py
+++ satscons/BoostUnitTests.py
@@ -19,10 +19,11 @@ def BoostUnitTests(env, target, source, test_source=None, LIBS = [], DEPENDS = [
     sources = sources + test_source
     binName = os.path.join(path,'.' + os.path.splitext(name)[0]+'.bin')
     testRunner = testEnv.Program(binName, sources)
+    stamp = os.path.join(path,'.' + os.path.splitext(name)[0]+'.stamp')
     if DEPENDS:
         env.Depends(testRunner, DEPENDS)
-    return env.Command(target, testRunner,
-                       [ './$SOURCE $BOOSTTESTARGS | tee  $TARGET' ])
+    return env.Command([ target, stamp ], testRunner,
+                       [ '( $SOURCE $BOOSTTESTARGS 2>&1 && touch ${TARGETS[1]} ) | tee ${TARGETS[0]}' ])
 
 def dispatcher(*arg,**kw):
     return BoostUnitTests(*arg,**kw)
