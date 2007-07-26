7537db67d400a0caa7e9f40b939653b3f1e2708f
g0dil <g0dil@wiback.org>
2007-07-26 10:58:44 +0000

    Add helper to wrap long (template) class labels in dot graphs

---
+++ doclib/dot-munge.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl -i -n
+
+# Reduce font size
+s/fontsize=10/fontsize=8/g; 
+
+# Wrap long labels (templates)
+if (/label=\"([^"]*)\"/) {              		    #"])){ # To make emacs happy ...
+    $pre=$`; 
+    $post=$'; 			                            #'     # To make emacs happy ...
+    $label=$1;
+
+    # Break at each komma
+    $label=~s/,/,\\r\\ \\ \\ \\ \\ \\ \\ \\ /g; 
+
+    # If more than one '<' is in the label, break after each '<'
+    if (($label=~tr/</</)>1) { 
+	$label=~s/</<\\r\\ \\ \\ \\ \\ \\ \\ \\ /g;
+    }
+
+    # If at least one break is in there ...
+    if ($label=~/\\r/) {
+	# Make last line flush right
+	$label.="\\r";
+	# and first line flush left
+	$label=~s/\\r/\\l/;
+    }
+    print "${pre}label=\"${label}\"${post}";
+} else { 
+    print;
+}
