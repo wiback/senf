00f35d39770bb1edd14e58169a7535677980b0ed
g0dil <g0dil@wiback.org>
2008-04-08 15:24:59 +0000

    doclib: Revert dependency tracking changes ...

--- doclib/yaptu.py
+++ doclib/yaptu.py
@@ -84,10 +84,9 @@ _RE_BEGIN = re.compile('{{')
 _RE_END = re.compile('}}')
 _RE_CONT = re.compile(r'\|\|')
 
-def process(text,*args,**kw):
+def process(text,*args):
     vardict = {}
     for arg in args : vardict.update(arg)
-    vardict.update(kw)
     output = StringIO()
     c = copier(_RE_EXPR, vardict, _RE_BEGIN, _RE_END, _RE_CONT,
                ouf = output)
