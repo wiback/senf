7eccff90698cf9ea8360b5b6aa5ebc38f43a9d54
dw6 <dw6@wiback.org>
2008-04-09 16:28:06 +0000

    add on-off switch module.

---
+++ PPI/ForwardSwitch.hh
@@ -0,0 +1,52 @@
+// Copyright (C) 2007
+// Fraunhofer Institute for Open Communication Systems (FOKUS)
+// Competence Center NETwork research (NET), St. Augustin, GERMANY
+//     David Wagner <dw6@berlios.de>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the
+// Free Software Foundation, Inc.,
+// 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+#ifndef FORWARDSWITCH_HH_
+#define FORWARDSWITCH_HH_
+
+// Custom includes
+#include "Connectors.hh"
+#include "Module.hh"
+
+namespace senf {
+namespace ppi {
+namespace module {
+
+class ForwardSwitch
+    : public Module
+{
+    SENF_PPI_MODULE(ForwardSwitch);
+public:
+
+    connector::ActiveInput<> input;
+    connector::ActiveOutput<> output;
+
+    ForwardSwitch(bool state);
+
+private:
+	bool forward_;
+    bool forward();
+    bool forward(bool state);
+    void stopForwarding();
+    void startForwarding();
+};
+
+}}} //namespaces
+#endif /*FORWARDSWITCH_HH_*/
