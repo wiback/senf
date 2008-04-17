1ad9f535a77a123481282f02f4da2a967a77336e
tho <tho@wiback.org>
2008-04-17 09:26:03 +0000

    use unittest include from util!! /grrr!/

--- Console/Variables.test.cc
+++ Console/Variables.test.cc
@@ -34,7 +34,7 @@
 #include "Parse.hh"
 #include "ScopedDirectory.hh"
 
-#include <boost/test/auto_unit_test.hpp>
+#include "../Utils/auto_unit_test.hh"
 #include <boost/test/test_tools.hpp>
 
 #define prefix_
