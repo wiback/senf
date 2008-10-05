6be2e607045ef04e6f3abb67d1a0f9d6a499cc87
g0dil <g0dil@wiback.org>
2008-10-05 18:28:08 +0000

    Scheduler: BUGFIX: Include local auto_unit_test header

--- Scheduler/EventManager.test.cc
+++ Scheduler/EventManager.test.cc
@@ -32,7 +32,7 @@
 #include "TimerEvent.hh"
 #include "SignalEvent.hh"
 
-#include <boost/test/auto_unit_test.hpp>
+#include "../Utils/auto_unit_test.hh"
 #include <boost/test/test_tools.hpp>
 
 #define prefix_
