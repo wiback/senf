e3c92e90b1ad50d49fbc1e6e2b1c9a8a7ef0ff9c
g0dil <g0dil@wiback.org>
2009-04-06 11:01:47 +0000

    Add debian/install-depends.sh utility

---
+++ debian/install-depends.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ `whoami` != root ]; then
+    echo "Must be root."
+    exit 2
+fi
+
+base="`dirname "$0"`"; base="`cd "$base"; pwd`"
+
+aptitude update
+aptitude install grep-dctrl
+
+packages="$(grep-dctrl -ns Build-Depends senf <"$base/control" \
+	| tr ',\n\t' '   '  \
+	| sed -e 's/([^)]*)//g' -e 's/ \+/ /g' -e 's/ $//')"
+
+aptitude install $packages
