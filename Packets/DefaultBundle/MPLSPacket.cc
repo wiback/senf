78027db7c4290eb48f3b142ce4377fc444b0bd80
cni <cni@wiback.org>
2009-06-29 13:39:59 +0000

    Packets/DefaultBundle: added MPLS header

---
+++ Packets/DefaultBundle/MPLSPacket.cc
@@ -0,0 +1,71 @@
+// $Id$
+//
+// Copyright (C) 2006
+// Fraunhofer Institute for Open Communication Systems (FOKUS)
+// Competence Center NETwork research (NET), St. Augustin, GERMANY
+//     Christian Niephaus <cni@berlios.de>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the
+// Free Software Foundation, Inc.,
+// 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+/** \file
+    \brief MPLSPacket non-inline non-template implementation */
+
+#include "MPLSPacket.hh"
+//#include "MPLSPacket.ih"
+
+// Custom includes
+#include <iomanip>
+#include <boost/io/ios_state.hpp>
+
+#define prefix_
+///////////////////////////////cc.p////////////////////////////////////////
+// senf::MPLSPacketType
+
+prefix_ void senf::MPLSPacketType::dump(packet p, std::ostream & os)
+{
+    boost::io::ios_all_saver ias(os);
+    os << "MPLS Header: \n"
+       << "  label : " << p->label() << "\n"
+       << "  traffic class : " << p->tc() << "\n"
+       << "  bottom of stack : " << p->s() << "\n"
+       << "  Time-to-live : " << p->ttl() << "\n";
+}
+
+prefix_ senf::PacketInterpreterBase::factory_t senf::MPLSPacketType::nextPacketType(packet p)
+{
+    if (p->s()) //last MPLS header
+        return no_factory();
+    else
+        return MPLSPacket::factory();
+}
+
+prefix_ void senf::MPLSPacketType::finalize(packet p)
+{
+}
+
+///////////////////////////////cc.e////////////////////////////////////////
+#undef prefix_
+
+
+// Local Variables:
+// mode: c++
+// fill-column: 100
+// c-file-style: "senf"
+// indent-tabs-mode: nil
+// ispell-local-dictionary: "american"
+// compile-command: "scons -u test"
+// comment-column: 40
+// End:
---
+++ Packets/DefaultBundle/MPLSPacket.test.cc
@@ -0,0 +1,98 @@
+// $Id$
+//
+// Copyright (C) 2006
+// Fraunhofer Institute for Open Communication Systems (FOKUS)
+// Competence Center NETwork research (NET), St. Augustin, GERMANY
+//     Christian Niephaus <cni@berlios.de>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the
+// Free Software Foundation, Inc.,
+// 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+/** \file
+    \brief MPLSPacket unit tests */
+
+//#include "MPLSPacket.test.hh"
+//#include "MPLSPacket.test.ih"
+
+// Custom includes
+#include "MPLSPacket.hh"
+
+
+#include "../../Utils/auto_unit_test.hh"
+#include <boost/test/test_tools.hpp>
+
+#define prefix_
+///////////////////////////////cc.p////////////////////////////////////////
+
+BOOST_AUTO_UNIT_TEST(mplsPacket_parse)
+{
+    senf::PacketData::byte data[] = {
+        0x00, 0x3e, 0x90, 0x80,  //1st mpls header
+        0x00, 0x3e, 0xb1, 0x80,  // last mpls header
+        0x00    //data
+    };
+    senf::MPLSPacket p (senf::MPLSPacket::create(data));
+
+    BOOST_CHECK_EQUAL( p->label(), 1001u);
+    BOOST_CHECK_EQUAL( p->tc(), 0u );
+    BOOST_CHECK( !p->s());
+    BOOST_CHECK_EQUAL( p->ttl(), 128u );
+
+    std::ostringstream oss (std::ostringstream::out);
+    SENF_CHECK_NO_THROW( p.dump( oss));
+}
+
+BOOST_AUTO_UNIT_TEST(mplsPacket_parse_chain)
+{
+    senf::PacketData::byte data[] = {
+        0x00, 0x3e, 0x90, 0x80,  //1st mpls header
+        0x00, 0x3e, 0xb1, 0x80,  // last mpls header
+        0x00    //data
+    };
+
+    senf::MPLSPacket p (senf::MPLSPacket::create(data));
+
+    BOOST_REQUIRE( p.next().is<senf::MPLSPacket>() );
+    senf::MPLSPacket p2 (p.next().as<senf::MPLSPacket>());
+    BOOST_CHECK( p2->s());
+    BOOST_REQUIRE( p2.next().is<senf::DataPacket>() );
+}
+
+BOOST_AUTO_UNIT_TEST(mplsPacket_create)
+{
+    senf::MPLSPacket p (senf::MPLSPacket::create());
+    p->label()=4444u;
+    p->ttl()=10u;
+    p->s()=true;
+    p->tc()=0x0u;
+
+    SENF_CHECK_NO_THROW(p.finalizeThis());
+
+}
+
+
+///////////////////////////////cc.e////////////////////////////////////////
+#undef prefix_
+
+
+// Local Variables:
+// mode: c++
+// fill-column: 100
+// c-file-style: "senf"
+// indent-tabs-mode: nil
+// ispell-local-dictionary: "american"
+// compile-command: "scons -u test"
+// comment-column: 40
+// End:
