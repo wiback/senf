770519a61c698df3b53f3a32009c4890bec4b972
g0dil <g0dil@wiback.org>
2010-08-20 06:40:25 +0000

    Packets/80211Bundle: Fix missing le16toh/le32toh on older libc

---
+++ senf/Packets/80211Bundle/radiotap.c
@@ -0,0 +1,44 @@
+// $Id$
+//
+// Copyright (C) 2010
+// Fraunhofer Institute for Open Communication Systems (FOKUS)
+// Competence Center NETwork research (NET), St. Augustin, GERMANY
+//     Stefan Bund <g0dil@berlios.de>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the
+// Free Software Foundation, Inc.,
+// 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+# include <senf/autoconf.hh>
+#
+# ifndef HAVE_LE16TOH
+#     define le16toh(x) (x)
+# endif
+#
+# ifndef HAVE_LE32TOH
+#     define le32toh(x) (x)
+# endif
+#
+# include "radiotap/radiotap.c"
+
+
+// Local Variables:
+// mode: c++
+// fill-column: 100
+// comment-column: 40
+// c-file-style: "senf"
+// indent-tabs-mode: nil
+// ispell-local-dictionary: "american"
+// compile-command: "scons -u test"
+// End:
