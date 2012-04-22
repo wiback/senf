fe872dd4c2fb81b71845577e7cde231cd2ed7bae
mtk <mtk@wiback.org>
2012-04-22 17:32:42 +0000

    added another ctor to StatisticsData

--- senf/Utils/StatisticAccumulator.hh
+++ senf/Utils/StatisticAccumulator.hh
@@ -47,6 +47,8 @@ namespace senf {
              : min(other.min), avg(other.avg), max(other.max), stddev(other.stddev), count(other.count) {};
          StatisticsData()
              : min(0.0), avg(0.0), max(0.0), stddev(0.0), count(0) {};
+         StatisticsData( float singleValue)
+             : min(singleValue), avg(singleValue), max(singleValue), stddev(0.0), count(1) {};
 
          float min;
          float avg;
