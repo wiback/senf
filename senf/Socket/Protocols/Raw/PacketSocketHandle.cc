0712d70a52cc53578f7a782ad2230016deee165b
g0dil <g0dil@wiback.org>
2013-09-23 07:30:41 +0000

    add rxQueueDropped() protocol member to PacketSocketProtocol

--- senf/Socket/Protocols/Raw/PacketSocketHandle.cc
+++ senf/Socket/Protocols/Raw/PacketSocketHandle.cc
@@ -5,20 +5,20 @@
 //
 // The contents of this file are subject to the Fraunhofer FOKUS Public License
 // Version 1.0 (the "License"); you may not use this file except in compliance
-// with the License. You may obtain a copy of the License at 
+// with the License. You may obtain a copy of the License at
 // http://senf.berlios.de/license.html
 //
-// The Fraunhofer FOKUS Public License Version 1.0 is based on, 
+// The Fraunhofer FOKUS Public License Version 1.0 is based on,
 // but modifies the Mozilla Public License Version 1.1.
 // See the full license text for the amendments.
 //
-// Software distributed under the License is distributed on an "AS IS" basis, 
-// WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License 
+// Software distributed under the License is distributed on an "AS IS" basis,
+// WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
 // for the specific language governing rights and limitations under the License.
 //
 // The Original Code is Fraunhofer FOKUS code.
 //
-// The Initial Developer of the Original Code is Fraunhofer-Gesellschaft e.V. 
+// The Initial Developer of the Original Code is Fraunhofer-Gesellschaft e.V.
 // (registered association), Hansastraße 27 c, 80686 Munich, Germany.
 // All Rights Reserved.
 //
@@ -122,6 +122,17 @@ prefix_ void senf::PacketSocketProtocol::promisc(std::string const & interface, 
                      mode ? PACKET_ADD_MEMBERSHIP : PACKET_DROP_MEMBERSHIP,
                      &mreq, sizeof(mreq)) < 0)
         throw senf::SystemException();
+}
+
+prefix_ unsigned senf::PacketSocketProtocol::rxQueueDropped()
+    const
+{
+    struct ::tpacket_stats stats;
+    ::socklen_t len = sizeof(stats);
+    if (::getsockopt(fd(), SOL_PACKET, PACKET_STATISTICS,
+                     reinterpret_cast<char *>(&stats), &len) < 0)
+        SENF_THROW_SYSTEM_EXCEPTION("::getsocketopt(SOL_PACKET, PACKET_STATISTICS) failed");
+    return stats.tp_drops;
 }
 
 //-/////////////////////////////////////////////////////////////////////////////////////////////////
