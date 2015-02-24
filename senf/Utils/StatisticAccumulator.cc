8b382e322fab2362970a7a317f60131d3834444d
mtk <mtk@wiback.org>
2015-02-24 23:06:38 +0000

    StatsAccumulator output compression

--- senf/Utils/StatisticAccumulator.cc
+++ senf/Utils/StatisticAccumulator.cc
@@ -39,9 +39,9 @@ std::ostream & senf::operator<<(std::ostream & os, senf::StatisticsData const & 
 {
     if (_data.cnt > 0) {
         os << "(";
-        os << "min "  << format::eng(_data.min).setw().setprecision(5).showpoint().showbase();
-        os << ", "    << format::eng(_data.avg, _data.stddev).setw().setprecision(5).showpoint().showbase();
-        os << ",max " << format::eng(_data.max).setw().setprecision(5).showpoint().showbase();
+        os << "min "  << format::eng(_data.min).setw().setprecision(5);
+        os << ", "    << format::eng(_data.avg, _data.stddev).setw().setprecision(5);
+        os << ",max " << format::eng(_data.max).setw().setprecision(5);
         os << ",cnt " << _data.cnt;
         os << ")";
     } else { 
