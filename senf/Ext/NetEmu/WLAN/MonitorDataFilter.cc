5cbc52a58b8f136dc52cd9d4c899a4bb9652961e
mtk <mtk@wiback.org>
2015-06-23 17:23:44 +0000

    decreased SEQ# expiration time to 100ms

--- senf/Ext/NetEmu/WLAN/MonitorDataFilter.cc
+++ senf/Ext/NetEmu/WLAN/MonitorDataFilter.cc
@@ -45,7 +45,7 @@ const short DEFAULT_WLAN_NOISE=-110;
 
 #define NO_SEQ_NO INT_MAX
 #define REORDER_MAX 64
-#define SEQ_EXPIRE 500
+#define SEQ_EXPIRE 100
 
 //-/////////////////////////////////////////////////////////////////////////////////////////////////
 // senf::emu::MonitorDataFilterStatistics
