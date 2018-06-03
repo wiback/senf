7af7ca50fa0a4e7746649c281c0ccd5a23bc1400
mtk <mtk@wiback.org>
2018-06-03 20:40:49 +0000

    fix for possible mem leak in ModulationParameterRegistry::registerModulatio

--- senf/Ext/NetEmu/WirelessInterfaceAPI.cc
+++ senf/Ext/NetEmu/WirelessInterfaceAPI.cc
@@ -49,6 +49,7 @@ prefix_ senf::emu::ModulationParameter::ModulationParameter(std::string const & 
 prefix_ senf::emu::ModulationParameter::id_t senf::emu::ModulationParameterRegistry::registerModulation(ModulationParameter & parameter)
 {
     parameter.id = boost::uint32_t(parameter.linkTypeId) << 24 | boost::uint32_t(parameter.v_modulationId());
+    modulations_.erase(parameter.id);
     modulations_.insert( parameter.id, parameter.v_clone());
     return parameter.id;
 }
