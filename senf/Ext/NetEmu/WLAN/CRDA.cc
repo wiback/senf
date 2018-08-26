58aca1ec28e30f486e6a4f006043f7caf016fbc6
mtk <mtk@wiback.org>
2018-08-26 11:30:51 +0000

    removed noIR() flag again - does not help with out object buys issue...

--- senf/Ext/NetEmu/WLAN/CRDA.cc
+++ senf/Ext/NetEmu/WLAN/CRDA.cc
@@ -115,13 +115,11 @@ prefix_ senf::emu::CRDA::CRDA()
         worldRegDomain_.rules.insert(RegulatoryRule()
                                      .frequencyRange(2457000, 2482000)
                                      .maxBandwidth(20000)
-                                     .maxEIRP(3000)
-                                     .noIR(true));
+                                     .maxEIRP(3000) );
         worldRegDomain_.rules.insert(RegulatoryRule()
                                      .frequencyRange(2474000, 2494000)
                                      .maxBandwidth(20000)
-                                     .maxEIRP(3000)
-                                     .noIR(true));
+                                     .maxEIRP(3000) );
         worldRegDomain_.rules.insert(RegulatoryRule()
                                      .frequencyRange(3000000, 4000000)
                                      .maxBandwidth(40000)
@@ -138,26 +136,22 @@ prefix_ senf::emu::CRDA::CRDA()
                                      .frequencyRange(5250000, 5330000)
                                      .maxBandwidth(80000)
                                      .maxEIRP(3000)
-                                     .noIR(true)
                                      .cacTime(0)
                                      .dfsRequired(true) );
         worldRegDomain_.rules.insert(RegulatoryRule()
                                      .frequencyRange(5490000, 5730000)
                                      .maxBandwidth(160000)
                                      .maxEIRP(3000)
-                                     .noIR(true)
                                      .cacTime(0)
                                      .dfsRequired(true) );
         worldRegDomain_.rules.insert(RegulatoryRule()
                                      .frequencyRange(5735000, 5835000)
                                      .maxBandwidth(80000)
-                                     .maxEIRP(3000)
-                                     .noIR(true) );
+                                     .maxEIRP(3000) );
         worldRegDomain_.rules.insert(RegulatoryRule()
                                      .frequencyRange(5835000, 6100000)
                                      .maxBandwidth(80000)
-                                     .maxEIRP(3000)
-                                     .noIR(true) );
+                                     .maxEIRP(3000) );
         worldRegDomain_.rules.insert(RegulatoryRule()
                                      .frequencyRange(57240000, 63720000)
                                      .maxBandwidth(2160000)
